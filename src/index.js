b21bcf40c0a5343fb0a5ed1c806cafd402d5492b
HYEJINLEE <crdrst777@gmail.com>
2023-01-26 15:24:31 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * 이름과 나이를 출력해주는 함수.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @returns {string} name + age
+ */
+function hello(name, age) {
+  return `name : ${name} , age : ${age}`;
+}
+
+hello("picpal", 10);
